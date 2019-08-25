33397516e7d71dfe9236c1cbf7e2a287e98c417c
aroquev00 <aroquev00@users.noreply.github.com>
2019-08-24 22:12:48 -0500

    Work on reading and displaying data on webpage

--- server.js
+++ server.js
@@ -3,34 +3,46 @@ const Readline = require('@serialport/parser-readline');
 const port = new SerialPort('/dev/cu.wchusbserial14410', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));// Read the port data
 
-var dataGen;
+var dataRaw;
+var dataLuz;
+var dataAgua;
 
 port.on("open", () => {
     console.log('serial port open');
-});parser.on('data', data =>{
-    dataGen = data; 
+  });
+parser.on('data', data =>{ 
+  dataRaw = data.toString();
+  if ((dataRaw.split("#"))[0] == "Luz")
+  dataLuz = (dataRaw.split("#"))[1];
+  else {
+    dataAgua = (dataRaw.split("#"))[1];
+  }
 });
 
-var app = require('express')();
+var path = require('path');
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var htmlPath = path.join(__dirname, 'html');
 
+app.use(express.static(htmlPath));
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/html/index.html');
 });
-
 
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-      io.emit('chat message', dataGen);
+      io.emit('chat message', dataLuz);
     });
   });
 
 function myFunc() {
-    io.emit('dato luz', dataGen);
+    io.emit('dato luz', dataLuz);
+    io.emit('dato agua', dataAgua);
 }  
 
 setInterval(myFunc, 1500)
